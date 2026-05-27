5f8fdab629de63a1f7e9da4e9ce0a1b10bdde21d
Adam Foflonker <87928802+DnA-IntRicate@users.noreply.github.com>
2026-05-27 14:23:22 +0200

    environ.py: Switch to OpenKey() and use 'with' blocks
    RAP-7

--- src/raptor/core/environ.py
+++ src/raptor/core/environ.py
@@ -14,10 +14,10 @@ except ImportError:
 
 def get_system_env_var(name: str) -> Optional[str]:
     if PLATFORM_WIN32:
-        key = winreg.CreateKey(winreg.HKEY_LOCAL_MACHINE, r"System\CurrentControlSet\Control\Session Manager\Environment")
         try:
-            return winreg.QueryValueEx(key, name)[0]
-        except Exception:
+            with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"System\CurrentControlSet\Control\Session Manager\Environment") as key:
+                return winreg.QueryValueEx(key, name)[0]
+        except FileNotFoundError:
             return None
 
     return os.environ.get(name)
@@ -25,10 +25,10 @@ def get_system_env_var(name: str) -> Optional[str]:
 
 def get_user_env_var(name: str) -> Optional[str]:
     if PLATFORM_WIN32:
-        key = winreg.CreateKey(winreg.HKEY_CURRENT_USER, r"Environment")
         try:
-            return winreg.QueryValueEx(key, name)[0]
-        except Exception:
+            with winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Environment") as key:
+                return winreg.QueryValueEx(key, name)[0]
+        except FileNotFoundError:
             return None
 
     return os.environ.get(name)
