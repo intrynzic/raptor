9b2a9e0e7ce329134b24c77bd3b52177013f4408
Azaam <53663013+aZaamBie@users.noreply.github.com>
2026-06-16 21:33:03 +0200

    Fix(RAP-11): Implement a new way of finding the repo root dir
    Modified the repo_root() method to search for the target file (raptor.toml), starting from the current path of git.py, until target found or root of filesystem found without finding target.
    
    Removed the main method that was initially used for testing

--- src/raptor/core/git.py
+++ src/raptor/core/git.py
@@ -5,8 +5,6 @@ from raptor.core.process import run
 
 import os # Review(Adam): Should the whole os module be imported, or just the functions that are used? (os.path and os.getcwd)
 from os import path as os_path
-from os import getcwd as os_getcwd
-
 
 @cache
 def repo_root() -> Path:
@@ -15,12 +13,12 @@ def repo_root() -> Path:
 
     while True:
         if os_path.isfile(os_path.join(current, "raptor.toml")): # Check if the target file exists in the current path
-            return Path(current) #return the current path as a Path object
+            return Path(current) # Return the current path as a Path object
         
         parent = os_path.dirname(current) # Get the parent directory of the current path
 
         if parent == current: # The root of the filesystem reached without finding target.
-            raise FileNotFoundError(f"Could not find {target} in any parent directory of {os_getcwd()}")
+            raise FileNotFoundError(f"Could not find {target} in any parent directory of {os.getcwd()}")
         
         current = parent # Move up the parent directory and continue searching.
 
@@ -33,7 +31,3 @@ def git_dir() -> Path:
 @cache
 def hooks_dir() -> Path:
     return git_dir() / "hooks"
-
-## TESTING: The new repo_root() function.
-if __name__ == "__main__":
-    print("Repo root:", repo_root())
