e3831ca9bd3cac836f1f7cec85bff39df969ccb5
Azaam <53663013+aZaamBie@users.noreply.github.com>
2026-06-20 08:07:22 +0200

    Cleaned up the script using "UV run ruff" commands
    The python lint error has been cleared by using the "UV run ruff format" and "UV run ruff check --fix".
    Next up: transfer this repo_root() functionality to the top of fs.py

--- src/raptor/core/git.py
+++ src/raptor/core/git.py
@@ -1,26 +1,22 @@
+import os
 from functools import cache
+from os import path as os_path
 from pathlib import Path
 
-from raptor.core.process import run
-
-import os
-from os import path as os_path
 
 @cache
 def repo_root() -> Path:
-    target = "raptor.toml" # The root of the repository
-    current = os_path.abspath(os.getcwd()) # The absolute path of the current working directory
+    target = "raptor.toml"  # The root of the repository
+    current = os_path.abspath(os.getcwd())  # The absolute path of the current working directory
 
     while True:
-        if os_path.isfile(os_path.join(current, "raptor.toml")): # Check whether target file exists in the current path
+        if os_path.isfile(os_path.join(current, "raptor.toml")):  # Check whether target file exists in the current path
             return Path(current)
-        
-        parent = os_path.dirname(current) # Get the parent directory of the current path
+        parent = os_path.dirname(current)  # Get the parent directory of the current path
 
-        if parent == current: # The root of the filesystem reached without finding target.
+        if parent == current:  # The root of the filesystem reached without finding target.
             raise FileNotFoundError(f"Could not find {target} in any parent directory of {os.getcwd()}")
-        
-        current = parent # Move up the parent directory and continue searching.
+        current = parent  # Move up the parent directory and continue searching.
 
 
 @cache
